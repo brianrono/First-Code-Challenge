09edc34b06860bac9298d81711efcdc8eac66f29
brianrono <brianrono16@gmail.com>
2023-03-19 15:47:02 +0300

    Fixing bugs

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-//GRADES calculated as: A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40
+//GRADES calculated as: A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less than 40
 
 function calculateGrade() {
     const score = parseInt(document.getElementById("score").value);
 
     let grade;
 
-    if (score < 0 && score > 100){
+    if (score > 100 || score < 0){
         console.log("Invalid Score, Try again!");
     }
-
-    else if (score > 79 && score <= 100) {
-        return "A";
+   
+    
+    else if (score > 79) {
+        grade = "A";
     } 
 
     else if (score >= 60 && score <= 79) {
-        return "B";
+        grade = "B";
     }
 
      else if (score >= 50 && score <= 59) {
-        return  "C";
+        grade = "C";
     } 
 
     else if (score >= 40 && score <= 49) {
-        return "D";
+        grade = "D";
     } 
 
     else {
-        return "E";
+        grade = "E";
     }
- 
+    console.log(calculateGrade);
     document.getElementById("result").innerHTML = `Student grade is: ${grade}`;
 }
-
-
 
 
 function checkSpeed() {
 	const speedInput = document.getElementById("speed-input");
 	const speed = Number(speedInput.value);
-	const speedLimit = 70;
-	const kmPerPoint = 5;
+	const speedLimit = 70;//it checks if the value of speed is less than or equal to speedLimit. If it is, it sets the "output" to "Ok".
+	const kmPerPoint = 5;//this constant variable represents the number of points charged per km exceeding the speed limit
     
 
-	if (speed <= speedLimit) {
+	if (speed <= speedLimit) {//user expects the speed to be less or equal the speed limit
 		document.getElementById("output").textContent = "Ok";
 	} else {
-		const points = Math.floor((speed - speedLimit) / kmPerPoint);
-		if (points >= 12) {
+		const points = Math.floor((speed - speedLimit) / kmPerPoint);//for every 5kmperhour exceeding the speed limit, user is charged 1 point for it
+		if (points >= 12) {//if the points exceeds 12, the license of the user is suspended
 			document.getElementById("output").textContent = "License suspended";
 		} else {
 			document.getElementById("output").textContent = `Points: ${points}`;
 		}
 	}
 }
+
+
